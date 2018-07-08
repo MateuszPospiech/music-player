53b4af97a5638450a77543d1bb097fdaaf5c4651
MateuszPospiech <pospiechmateusz@gmail.com>
2018-07-09 00:36:21 +0200

    Added hover efect on nav buttons and click efect on next and prev button

--- src/js/script.js
+++ src/js/script.js
@@ -10,9 +10,35 @@ const viewMore = document.getElementById('header__view-more');
 
 const viewMoreActive = () => {
     viewMore.classList.toggle('header__view-more--active');
-    console.log('123');
 }
 
 header__icon.addEventListener('click', viewMoreActive);
 viewMore__button.addEventListener('click', viewMoreActive);
 //END View more section
+
+/***Button panel section***/
+const buttonPanel__prev = document.querySelector('.button-panel__prev');
+const buttonPanel__next = document.querySelector('.button-panel__next');
+
+
+const songNavActivePrev = () => {
+    buttonPanel__prev.classList.add('button-panel__song-nav--active');
+}
+const songNavActiveNext = () => {
+    buttonPanel__next.classList.add('button-panel__song-nav--active');
+}
+
+//buttonPanel__prev.forEach(buttonPanel__prev => buttonPanel__prev.addEventListener('click', songNavActive));
+buttonPanel__prev.addEventListener('click', songNavActivePrev);
+buttonPanel__next.addEventListener('click', songNavActiveNext);
+
+//Remove class ACTIVE
+function removeTransition (e) {
+    if (e.propertyName !== 'transform') return; //skip if it's not transform
+    this.classList.remove('button-panel__song-nav--active');
+}
+
+buttonPanel__prev.addEventListener('transitionend', removeTransition);
+buttonPanel__next.addEventListener('transitionend', removeTransition);
+
+/*END Button panel section*/
